74e18670759cb9737fc4bd1bf415ee1f905579aa
Adam Mann <aemann2@gmail.com>
2021-04-22 15:44:07 -0700

    added browser local storage

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from 'react';
+// importing useEffect to aid with setting browser local storage
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
@@ -6,7 +7,16 @@ export const BookContext = createContext();
 const BookContextProvider = (props) => {
   // we're now using the dispatch to set the state (rather than setBooks)
   // for useReducer, we pass in 2 params: the reducer we want to use, and the initial value of the piece of state
-  const [books, dispatch] = useReducer(bookReducer, []);
+  // the third argument is for localStorage. it sets the state to what's in localStorage (if anything is there)
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    // every time the books state changes, we reset "books" in local storage with the stringified array
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
 
   return (
     // we're providing books (because it's a state), and also the dispatch, because that's what we use to set the state of the books. with the dispatch passed down through the value, all the children will now have access to the state changing methods that live in the reducer
